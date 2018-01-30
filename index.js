873d8d134f68fc3bd27f35bd7537c2716b07a2e0
Janusz Szubert <theinvalidnonce@gmail.com>
2018-01-30 00:57:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,11 +6,10 @@ function driversWithRevenueOver(arr, revenue) {
 
 function driverNamesWithRevenueOver(arr, revenue) {
   return arr.filter(function (el) {
-    for (const name in arr) {
+    let newArr = []
       if (el['revenue'] > revenue) {
-        // debugger;
-        return [el.name]
+        newArr.push(el['name'])
       }
-    }
+    return newArr
   })
 }
